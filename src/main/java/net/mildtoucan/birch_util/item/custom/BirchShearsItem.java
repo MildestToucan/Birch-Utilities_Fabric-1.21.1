b22e9662a1fa50c633ec20cf7babfc9fcff5c6b7
MildToucan <introvertpale@gmail.com>
2025-05-11 18:02:17 +0200

    Added Birch Shears interaction with pumpkin blocks
    Usage of mixins was avoided via the context.getThing Methods. This will make the shears as compatible as possible!

--- src/main/java/net/mildtoucan/birch_util/item/custom/BirchShearsItem.java
+++ src/main/java/net/mildtoucan/birch_util/item/custom/BirchShearsItem.java
@@ -1,10 +1,14 @@
 package net.mildtoucan.birch_util.item.custom;
 
 
+import net.mildtoucan.birch_util.item.ModItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.CarvedPumpkinBlock;
+import net.minecraft.block.PumpkinBlock;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.BoggedEntity;
 import net.minecraft.entity.passive.MooshroomEntity;
@@ -13,17 +17,20 @@ import net.minecraft.entity.passive.SnowGolemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.item.Items;
 import net.minecraft.item.ShearsItem;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import net.minecraft.world.event.GameEvent;
-
-import java.util.Map;
 
 import static net.minecraft.entity.LivingEntity.getSlotForHand;
 
@@ -34,14 +41,6 @@ public class BirchShearsItem extends ShearsItem {
         super(settings.maxDamage(595) //About 2.5x more durability than regular shears.
                 .component(DataComponentTypes.TOOL, ShearsItem.createToolComponent()));
     }
-
-
-    //This is using the same system as the chisel tool in KaupenJoe's 1.21 Fabric Tutorial.
-    //Very unoptimized for a single block, will need to refine this into a better solution later.
-    public static final Map<Block, Block> PUMPKIN_SHEAR_MAP =
-            Map.of(
-                    Blocks.PUMPKIN, Blocks.CARVED_PUMPKIN
-            );
 
     @Override //This Override essentially is for shearing entities.
     public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
@@ -84,27 +83,42 @@ public class BirchShearsItem extends ShearsItem {
         return ActionResult.PASS;
     }// tyyyyyyyyyyyyy Message courtesy of my cat Hety
 
+
+
     @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
         World world = context.getWorld();
         Block clickedBlock = world.getBlockState(context.getBlockPos()).getBlock();
-        //Those two lines grab info on the world to determine the block being clicked on
-        if(!world.isClient()) {
-            if(PUMPKIN_SHEAR_MAP.containsKey(clickedBlock)) {
-                world.setBlockState(context.getBlockPos(), PUMPKIN_SHEAR_MAP.get(clickedBlock).getDefaultState());
+        BlockPos pos = context.getBlockPos();
+        PlayerEntity player = context.getPlayer();
 
-                context.getStack().damage(1, ((ServerWorld) world), ((ServerPlayerEntity) context.getPlayer()),
+        //^^ Grabs info about the clicked block, creating it and the block into a variable we can use.
+        //This is done to replicate the onUseWithItem parameters of the blocks we're interacting with.
+        if(!world.isClient()) {
+            if(clickedBlock instanceof PumpkinBlock) {
+                Direction direction = context.getSide();
+                Direction direction2 = direction.getAxis() == Direction.Axis.Y ? player.getHorizontalFacing().getOpposite() : direction;
+                world.playSound(null, pos, SoundEvents.BLOCK_PUMPKIN_CARVE, SoundCategory.BLOCKS, 1.0f, 1.0f);
+                world.setBlockState(pos, Blocks.CARVED_PUMPKIN.getDefaultState().with(CarvedPumpkinBlock.FACING, direction2), Block.NOTIFY_ALL_AND_REDRAW);
+
+                ItemEntity itemEntity = new ItemEntity(
+                        world,
+                        pos.getX() + 0.5 + direction2.getOffsetX() * 0.65,
+                        pos.getY() + 0.1,
+                        pos.getZ() + 0.5 + direction2.getOffsetZ() * 0.65,
+                        new ItemStack(Items.PUMPKIN_SEEDS, 4)
+                );
+                itemEntity.setVelocity(
+                        0.05 * direction2.getOffsetX() + world.random.nextDouble() * 0.02, 0.05, 0.05 * direction2.getOffsetZ() + world.random.nextDouble() * 0.02
+                );
+                world.spawnEntity(itemEntity);
+
+                context.getStack().damage(1,((ServerWorld) world), ((ServerPlayerEntity) context.getPlayer()),
                         item -> context.getPlayer().sendEquipmentBreakStatus(item, EquipmentSlot.MAINHAND));
 
-                world.playSound(null, context.getBlockPos(), SoundEvents.ENTITY_SHEEP_SHEAR, SoundCategory.PLAYERS);
-                return ActionResult.SUCCESS;
-            } else if(clickedBlock.equals(Blocks.BEEHIVE) || clickedBlock.equals(Blocks.BEE_NEST)) {
-
+                world.emitGameEvent(player, GameEvent.SHEAR, pos);
             }
         }
-
-
-
-        return ActionResult.PASS;
+        return ActionResult.SUCCESS;
     }
 }
